22df6349a8e9ba2b274ff8bd225804811c70d70c
hggnomm <hggnomm.dev@gmail.com>
2024-11-07 16:48:52 +0700

    Add type

--- components/StartupCard.tsx
+++ components/StartupCard.tsx
@@ -4,12 +4,15 @@ import Image from "next/image";
 import Link from "next/link";
 import { describe } from "node:test";
 import { Button } from "./ui/button";
+import { Author, Startup } from "@/sanity/types";
+
+export type StartupTypeCard = Omit<Startup, "author"> & { author?: Author };
 
 const StartupCard = ({ post }: { post: StartupTypeCard }) => {
   const {
     _createdAt,
     views,
-    author: { _id: authorId, name },
+    author,
     title,
     category,
     _id,
@@ -28,13 +31,13 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
 
       <div className="flex-between mt-5 gap-5">
         <div className="flex-1">
-          <Link href={`/user/${authorId}`} />
+          <Link href={`/user/${author?.id}`} />
           <Link href={`/startup/${_id}`}>
-            <h3 className="text-26-semibold line-clamp-1">{name}</h3>
+            <h3 className="text-26-semibold line-clamp-1">{author?.name}</h3>
           </Link>
           <p className="text-16-medium line-clamp-1">{title}</p>
         </div>
-        <Link href={`/user/${authorId}`}>
+        <Link href={`/user/${author?.id}`}>
           <Image
             src={"https://placehold.co/600x400"}
             alt="placeholer"
@@ -50,7 +53,7 @@ const StartupCard = ({ post }: { post: StartupTypeCard }) => {
       </Link>
 
       <div className="flex-between gap-3 mt-5">
-        <Link href={`/?query=${category.toLowerCase()}`}>
+        <Link href={`/?query=${category?.toLowerCase()}`}>
           <p className="text-16-medium">{category}</p>
         </Link>
         <Button className="startup-card_btn" asChild>
